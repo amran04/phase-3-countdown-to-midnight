86c56beaa944840b638db3b6557f13b95c5b4eef
Amranabdi1 <amranabdikadir101@gmail.c0m>
2022-10-25 12:15:11 +0300

    completed assignment

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,13 @@
 #write your code here
+def countdown int
+    i = 0 
+    while i<int
+        puts "#{int-i} SECOND(S)!"
+        i+=1
+    end
+    "HAPPY NEW YEAR!"
+end
 
-def countdown
+def countdown_with_sleep countdown
+    sleep countdown
 end
